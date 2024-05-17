f33e7f295dfad7ce3451abefcb995411d4001d58
poriia <poriia@gmail.com>
2024-05-17 11:17:14 +0330

    improve abort function

--- public/views/403.view.php
+++ public/views/403.view.php
@@ -2,7 +2,7 @@
 <?php require("partials/nav.php"); ?>
 <main>
     <div class="mx-auto max-w-7xl py-6 sm:px-6 lg:px-8">
-        Not Found!
+        Unauthorize!
         <p>
             <a href="/" class="text-blue-500">back to home</a>
         </p>
